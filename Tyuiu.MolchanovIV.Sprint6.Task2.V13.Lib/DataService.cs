856f2f98b4c427807d07ac6676445b23eb677bf5
frmeister <frmeister1337@gmail.com>
2025-12-11 22:43:01 +0500

    Task2

--- Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib/DataService.cs
+++ Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib/DataService.cs
@@ -13,7 +13,9 @@ namespace Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib
             for (int i = startValue; i <= stopValue; i++)
             {
                 if (2 * i / 3 == 0)
-                { res[i] = 0;  continue; }
+                {
+                    res[i] = 0;  continue; 
+                }
                 res[i] = Math.Round(Math.Sin(i) + 2 * i / 3 - Math.Cos(i) * 4 * i, 2);
             }
 
