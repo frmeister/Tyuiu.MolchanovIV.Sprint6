aa791fa47a63cf9226dd7f291bb4b1ba9398169c
frmeister <frmeister1337@gmail.com>
2025-12-12 19:53:40 +0500

    Task2 fix all

--- Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib/DataService.cs
+++ Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib/DataService.cs
@@ -6,20 +6,25 @@ namespace Tyuiu.MolchanovIV.Sprint6.Task2.V13.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            int arrLen = stopValue - startValue + 1;
+            int arrLen = stopValue - startValue + 1, curValue = startValue;
 
             double[] res = new double[arrLen];
 
             for (int i = 0; i < arrLen; i++)
             {
-                if (2 * i / 3 == 0)
-                {
-                    res[i] = 0;  continue; 
-                }
-                res[i] = Math.Round(Math.Sin(i) + 2 * i / 3 - Math.Cos(i) * 4 * i, 2);
+                res[i] = Math.Sin(curValue);
+                res[i] += 2 * curValue / 3;
+                res[i] -= Math.Cos(curValue) * 4 * curValue;
+                res[i] = Math.Round(res[i], 2);
+
+                curValue++;
             }
 
             return res;
         }
     }
 }
+/*
+ real: [0.0,0.0,5.24,14.02,11.7,-3.63,-19.32,-16.45,10.65,39.21,39.02]
+expected: [3.63,-11.7,-14.02,-5.24,1.32,0.0,-1.32,5.24,14.02,11.7,-3.63]
+ */
